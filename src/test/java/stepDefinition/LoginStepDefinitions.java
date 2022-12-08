64c2f47870598cd5f1d0e50851d8107bd2590707
Frank Atencio <frank.atencio.loreth@gmail.com>
2022-12-08 17:09:53 -0500

    configuracion del stepdefinitions y el runner

--- src/test/java/stepDefinition/LoginStepDefinitions.java
+++ src/test/java/stepDefinition/LoginStepDefinitions.java
@@ -1,10 +1,19 @@
 package stepDefinition;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
 
 public class LoginStepDefinitions {
+
+    @Before
+    public void setUp() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+
     @Dado("^El cliente abre la página web$")
     public void elClienteAbreLaPaginaWeb() {
     }
